ed6a4da56b979af5dd73183df28a13a738d5ef9e
h-nagata <h-nagata@172.19.2.9>
2014-07-17 14:52:51 +0900

    テーブル定義を更新

--- pim/apps/common/models.py
+++ pim/apps/common/models.py
@@ -2,14 +2,6 @@
 from django.db import models
 from django.contrib import admin
 
-class Team(models.Model):
-    """
-    チームモデル
-    """
-    def __unicode__(self):
-        return self.name
-    # チーム名
-    name = models.CharField(max_length=200)    
     
 class Living_situation(models.Model):
     """
@@ -19,6 +11,24 @@ class Living_situation(models.Model):
         return self.living_situation
     # 暮らし状況
     living_situation = models.CharField(max_length=200)
+
+class Type(models.Model):
+    """
+    性格
+    """
+    def __unicode__(self):
+        return self.type
+    # 性格
+    type = models.CharField(max_length=200)
+    
+class Level(models.Model):
+    """
+    性格
+    """
+    def __unicode__(self):
+        return self.level
+    # レベル説明
+    level_description = models.CharField(max_length=50)
 
 class Target(models.Model):
     """
@@ -34,6 +44,8 @@ class Target(models.Model):
     name_en = models.CharField(max_length=200)
     # 難易度
     difficulty = models.IntegerField()
+    #レベル
+    level = models.ForeignKey(Level)
     # 接触の目的
     purpose = models.CharField(max_length=50)
     # 会った日
@@ -63,13 +75,44 @@ class Target(models.Model):
     # 備考
     remarks = models.TextField(blank=True)
     # 性格1
-    type_1 = models.CharField(max_length=200)
+    type_1 = models.ForeignKey(Type, related_name='type_1')
     # 性格2
-    type_2 = models.CharField(max_length=200)
+    type_2 = models.ForeignKey(Type, related_name='type_2')
     # 性格3
-    type_3 = models.CharField(max_length=200)
+    type_3 = models.ForeignKey(Type, related_name='type_3')
     # 終了フラグ
     done_flg = models.BooleanField(default=False)
+
+class Introduce(models.Model):
+    """
+    紹介者管理
+    """
+    def __unicode__(self):
+        return self.introuce
+    #紹介された人
+    target = models.ForeignKey(Target, related_name='target')
+    #紹介者
+    introduced_by = models.ForeignKey(Target, related_name='introduced_by')
+
+class Target_relationship(models.Model):
+    """
+    ターゲット同士の関係性
+    """
+    #ターゲット1
+    target1 = models.ForeignKey(Target, related_name='target1')
+    #ターゲット2
+    target2 = models.ForeignKey(Target, related_name='target2')
+    #関係性
+    relationship = models.CharField(max_length=200)
+    
+class Team(models.Model):
+    """
+    チームモデル
+    """
+    def __unicode__(self):
+        return self.name
+    # チーム名
+    name = models.CharField(max_length=200)    
 
 class User (models.Model):
     """
@@ -78,7 +121,7 @@ class User (models.Model):
     def __unicode__(self):
         return self.name_kanji
     # ユーザID
-    user_id = models.CharField(max_length=6, primary_key=True)
+    login_id = models.CharField(max_length=20, primary_key=True)
     # 名前（漢字）
     name_kanji = models.CharField(max_length=200, blank=True)
     # 名前（ひらがな）
@@ -91,30 +134,49 @@ class User (models.Model):
     team = models.ForeignKey(Team)
     # 出身地
     birthday = models.DateField(blank=True)
+    #出身地
+    birth_place = models.CharField(max_length=200)
     # 対象性格1
-    target_type_1 = models.CharField(max_length=200)
+    target_type_1 = models.ForeignKey(Type, related_name='target_type_1')
     # 対象性格2
-    target_type_2 = models.CharField(max_length=200)
+    target_type_2 = models.ForeignKey(Type, related_name='target_type_2')
     # 対象性格3
-    target_type_3 = models.CharField(max_length=200)
+    target_type_3 = models.ForeignKey(Type, related_name='target_type_3')
+
     
-class Responsible(models.Model):
+class Progress(models.Model):
+    """
+    進捗マスタ
+    """
+    def __unicode__(self):
+        return self.progress
+    #進捗名
+    progress_name = models.CharField(max_length=50)
+    
+class Progress_management(models.Model):
     """
     担当者定義モデル
     """
     def __unicode__(self):
-        return self.responsible_by
+        return self.progress_management
     # 顧客
     target = models.ForeignKey(Target)
     # 担当者
     responsible_by = models.ForeignKey(User)
     # 関係性。友人:1,取引相手:2,上司:3,会社同期:4,会社後輩:5,大学同期:6,大学先輩:7,大学後輩:8,地元:9,幹事仲間:10,恋人:11,ほぼ知らない:12
-    relationship = models.IntegerField()
+    relationship = models.CharField(max_length=200)
     #進捗状況
-    progress_status = models.TextField(blank=True)
-    
-admin.site.register(Target)
+    progress = models.ForeignKey(Progress)
+    #備考
+    remarks = models.CharField(max_length=200)
+
 admin.site.register(Living_situation)
-admin.site.register(User)
+admin.site.register(Type)
+admin.site.register(Level)
+admin.site.register(Target)
+admin.site.register(Introduce)
+admin.site.register(Target_relationship)
 admin.site.register(Team)
-admin.site.register(Responsible)
+admin.site.register(User)
+admin.site.register(Progress)
+admin.site.register(Progress_management)
