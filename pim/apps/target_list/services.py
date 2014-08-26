2559f8a91c9592b0b5ae23ba37cf0427fa8e0a48
h-nagata <h-nagata@172.19.2.9>
2014-08-26 15:10:28 +0900

    ターゲット登録フォームにエラーチェックを追加。

--- pim/apps/target_list/services.py
+++ pim/apps/target_list/services.py
@@ -1,6 +1,10 @@
 import forms as FORMS
 
 def addTarget(request):
+    print 'addTarget'
     new_target_form = FORMS.TargetForm(request.POST)
     if new_target_form.is_valid():
         new_target_form.save()
+        return 'success'
+    else:
+        return 'fail'
--- pim/apps/target_list/views.py
+++ pim/apps/target_list/views.py
@@ -3,31 +3,42 @@ from django.core.context_processors import csrf
 import configs as CONFIG
 import forms as FORMS
 import services as SERVICES
+import messages as MSGS
 
 # targetlist views
 
 def index(request):
     addForm = FORMS.TargetForm()
+    forms = {'addForm':addForm}
+    c = {}    
+    c.update(forms)
+    return show(request, c)
     
+def add(request):
+    if request.method == 'POST':
+        result = SERVICES.addTarget(request)
+        if not result == 'success':
+            fail_form = FORMS.TargetForm(request.POST)
+            c = {'addForm':fail_form}
+            c.update({'form_message':MSGS.ADD_FAIL})
+            return show(request, c)
+    c = {'form_message': MSGS.ADD_SUCCESS}
+    c.update({'addForm':FORMS.TargetForm()})
+    return show(request, c)
+
+def show(request, c):
     print 'target_list'    
     main_url = CONFIG.TOP_URL
     page_title = CONFIG.TARGET_LIST_PAGE_TITLE_URL
     main_content = CONFIG.TARGET_LIST_MAIN_URL
     sub_content = CONFIG.TARGET_LIST_SUB_URL
-    forms = {'addForm':addForm}
-    c = {}    
+
     url_dict = {'main_url':main_url, 
                 'page_title':page_title, 
                 'main_content':main_content,
                 'sub_content':sub_content}
-    action_dict = CONFIG.ACTION_DICT
     c.update(csrf(request))
     c.update(url_dict)
-    c.update(forms)
-    c.update(action_dict)
+    c.update(CONFIG.ACTION_DICT)
     return render(request, 'common/main.html', c)
     
-def add(request):
-    if request.method == 'POST':
-        SERVICES.addTarget(request)
-    return index(request)
