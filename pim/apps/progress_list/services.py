dd67c6208f4160f95a13b6d7128feac3d5848546
yu-yama <yu-yama@133.137.88.34>
2014-08-26 16:10:31 +0900

    #26 サービスクラス追加

---
+++ pim/apps/progress_list/services.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import forms as FORMS
+import common.models as MODELS
+
+def addProgress(request):
+    print 'addProgress'
+
+    print 'username = ' + request.user.username
+    # ユーザ名をセットしたUserクラスを作成
+    user = MODELS.User(username=request.user.username)
+    # responsible_byに作成したuserモデルをセットしてProgress_managementクラスを作成
+    progress_management = MODELS.Progress_management(responsible_by=user)
+    # progress_managementクラスを利用してフォームを作成
+    # http://docs.djangoproject.jp/en/latest/topics/forms/modelforms.html
+    new_progress_form = FORMS.ProgressManagementForm(request.POST, instance=progress_management)
+
+    print new_progress_form.errors
+    print new_progress_form
+    if new_progress_form.is_valid():
+        print 'is_valid'
+        new_progress_form.save()
+    else: 
+        print 'fail addProgress'
