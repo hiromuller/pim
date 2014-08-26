ac122431ea60b99cba1c8afe96d631a61af0e33b
h-nagata <h-nagata@172.19.2.9>
2014-08-26 18:54:30 +0900

    #14 ターゲット一覧機能

--- pim/apps/target_list/services.py
+++ pim/apps/target_list/services.py
@@ -1,10 +1,56 @@
+# -*- coding: utf-8 -*-
 import forms as FORMS
+import common.models as MODELS
+import logging
 
-def addTarget(request):
-    print 'addTarget'
-    new_target_form = FORMS.TargetForm(request.POST)
-    if new_target_form.is_valid():
-        new_target_form.save()
+logger = logging.getLogger('app')
+
+def addNewTarget(addTargetForm, user):
+    
+    result_flg = False
+    if isUserExist(user):
+        new_target = addTarget(addTargetForm)
+        if new_target:
+            addTargetRegister(new_target, user)
+            result_flg = True
+    
+    if result_flg:
         return 'success'
     else:
         return 'fail'
+    
+    
+def addTarget(request):
+
+    logger.info('登録')
+
+    new_target_form = FORMS.TargetForm(request.POST)
+
+    if new_target_form.is_valid():
+        target = new_target_form.save()
+        return target
+    else:
+        return None
+
+
+def addTargetRegister(target, user):
+
+    target_register = MODELS.Target_register()
+    target_register.target = target
+    target_register.user = user
+    target_register.save()
+
+
+def isUserExist(user):
+    logger.info('username = ' + user.username)
+
+    # 投げられたusernameで存在チェックを行う
+    isUserExist = MODELS.User.objects.filter(username=user.username).count()
+    if isUserExist == 0:
+        return False
+    else:
+        return True
+
+def getAllTargetList():
+    return MODELS.Target.objects.all().order_by('name_en')
+    
--- pim/apps/target_list/views.py
+++ pim/apps/target_list/views.py
@@ -1,33 +1,57 @@
+# -*- coding: utf-8 -*-
+# targetlist views
 from django.shortcuts import render
 from django.core.context_processors import csrf
 import configs as CONFIG
 import forms as FORMS
 import services as SERVICES
 import messages as MSGS
+import logging
 
-# targetlist views
+logger = logging.getLogger('app')
 
 def index(request):
-    addForm = FORMS.TargetForm()
-    forms = {'addForm':addForm}
+    """
+    ターゲット一覧画面インデックス
+    最初に必ず通る
+    """
+    logger.info('ターゲットリスト画面開始')
+    forms = {'addForm':FORMS.TargetForm()}
     c = {}    
     c.update(forms)
     return show(request, c)
-    
+
 def add(request):
+    """
+    ターゲット登録
+    """
+    logger.info('ターゲット登録')
+
     if request.method == 'POST':
-        result = SERVICES.addTarget(request)
+        addTargetForm = FORMS.TargetForm(request.POST)
+        result = SERVICES.addNewTarget(addTargetForm, request.user)
+
         if not result == 'success':
             fail_form = FORMS.TargetForm(request.POST)
             c = {'addForm':fail_form}
             c.update({'form_message':MSGS.ADD_FAIL})
+
             return show(request, c)
+
     c = {'form_message': MSGS.ADD_SUCCESS}
     c.update({'addForm':FORMS.TargetForm()})
     return show(request, c)
 
 def show(request, c):
-    print 'target_list'    
+    """
+    ターゲット一覧表示メソッド
+    URLなど必要な値をセットし、表示する
+    """
+    logger.info('ターゲットリスト表示')
+    
+    target_list = SERVICES.getAllTargetList()
+    c.update({'target_list':target_list})
+
     main_url = CONFIG.TOP_URL
     page_title = CONFIG.TARGET_LIST_PAGE_TITLE_URL
     main_content = CONFIG.TARGET_LIST_MAIN_URL
