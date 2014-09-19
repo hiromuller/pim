f5e4a919aade62cd7c0fda7fb15eac209cf7a0d9
yu-yama <yu-yama@133.137.88.52>
2014-09-19 20:39:02 +0900

    #58 進捗一覧画面：チームに所属していないと落ちる 対応

--- pim/apps/progress/services.py
+++ pim/apps/progress/services.py
@@ -58,17 +58,26 @@ def getTeamProgressList(user):
     ユーザからチームの別メンバの進捗を取得する
     return progress_management[]
     """
-    # 自分のメンバーシップ取得
-    my_membership = MODELS.Membership.objects.get(user=user)
-
-    #  チームが同一の自分以外のメンバーシップを取得
-    team_membership_list = MODELS.Membership.objects.filter(team=my_membership.team.pk).exclude(user=user)
-
-    # 自分以外のチームのメンバーシップのユーザに紐づく進捗をリストに詰める
     team_progress_lists = []
-    for team_membership in team_membership_list:
-        team_progress_list= MODELS.Progress_management.objects.filter(responsible_by=team_membership.user).order_by('target', 'registered_at')
-        team_progress_lists.append(team_progress_list)
+
+    # 自分のメンバーシップ取得
+    my_membership = MODELS.Membership.objects.filter(user=user)
+
+    # 1ユーザ1チームにしか対応していないため、とれた1件目のメンバーシップ取得
+    if len(my_membership) == 1:
+        my_membership = my_membership[0]
+
+        #  チームが同一の自分以外のメンバーシップを取得
+        team_membership_list = MODELS.Membership.objects.filter(team=my_membership.team).exclude(user=user)
+
+        # 自分以外のチームのメンバーシップのユーザに紐づく進捗をリストに詰める
+        for team_membership in team_membership_list:
+            team_progress_list= MODELS.Progress_management.objects.filter(responsible_by=team_membership.user).order_by('target', 'registered_at')
+            team_progress_lists.append(team_progress_list)
+
+    else:
+        # 自分のメンバーシップが1件以外の場合、何もしない
+        pass
 
     return team_progress_lists
 
